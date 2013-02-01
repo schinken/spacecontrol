f231a324634b1b211e812828c456303cc3ebcc48
schinken <schinken@hackerspace-bamberg.de>
2013-02-01 16:22:01 +0100

    added status cli tool

---
+++ status/status.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2
+
+import requests
+
+if __name__ == '__main__':
+
+    req = requests.get('http://status.bckspc.de/status.php?response=json')
+    try:
+        result = req.json()
+    except:
+        sys.exit('Invalid response')
+
+    print "Members present:", result['members']
+    print "Member devices:", result['member_devices']
+    print "Unknown devices:", result['unknown_devices']
+
+    if len(result['members_present']):
+
+        nicknames = []
+        for entry in result['members_present']:
+            nicknames.append(entry['nickname'])
+
+        print "Members:", ','.join(nicknames)
