c2843593c3f62100ee30fdbe9de47cafca907bb9
schinken <schinken@hackerspace-bamberg.de>
2013-02-01 21:26:12 +0100

    updated relais client, added status

--- webrelais/webrelais.py
+++ webrelais/webrelais.py
@@ -10,8 +10,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Switch WebRelais')
 
     mutex = parser.add_mutually_exclusive_group(required=True)
-    mutex.add_argument('--on',  '-on', dest='mode', action='store_const', const=True, help='Switch on')
-    mutex.add_argument('--off', '-off', dest='mode', action='store_const', const=False, help='Switch off')
+    mutex.add_argument('--on',  '-on', dest='mode', action='store_const', const='on', help='Switch on')
+    mutex.add_argument('--off', '-off', dest='mode', action='store_const', const='off', help='Switch off')
+    mutex.add_argument('--status', '-s', dest='mode', action='store_const', const='status', help='Switch off')
 
     parser.add_argument('relais', nargs='?', type=int, choices=range(8), help="Relais you want to switch")
 
@@ -19,9 +20,12 @@ if __name__ == '__main__':
 
     webrelais = RelaisClient(server, 443)
 
-    if args.mode:
+    if args.mode == 'on':
         webrelais.setPort(args.relais)
         print "Relais %d switched on" % (args.relais,)
-    else:
+    elif args.mode == 'off':
         webrelais.resetPort(args.relais)
         print "Relais %d switched off" % (args.relais,)
+    elif args.mode == 'status':
+        for relais, status in enumerate(webrelais.getPorts()):
+            print "Relais %d is %s" % (relais, ('on' if status else 'off'))
