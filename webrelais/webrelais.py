5c85586dba575432f2d406d1b930a868b4f06e21
schinken <schinken@hackerspace-bamberg.de>
2013-02-17 01:37:48 +0100

    added relais listing

--- webrelais/webrelais.py
+++ webrelais/webrelais.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     mutex.add_argument('--on',  '-on', dest='mode', action='store_const', const='on', help='Switch on')
     mutex.add_argument('--off', '-off', dest='mode', action='store_const', const='off', help='Switch off')
     mutex.add_argument('--status', '-s', dest='mode', action='store_const', const='status', help='Status of Relais')
+    mutex.add_argument('--list',  '-l', dest='mode', action='store_const', const='list', help='List relais aliases')
 
     parser.add_argument('relais', nargs='?', type=int, choices=range(8), help="Relais you want to switch")
 
@@ -29,3 +30,10 @@ if __name__ == '__main__':
     elif args.mode == 'status':
         for relais, status in enumerate(webrelais.getPorts()):
             print "Relais %d is %s" % (relais, ('on' if status else 'off'))
+    elif args.mode == 'list':
+        
+        relais = ['door open', 'door close', 'door buzzer', 'exit light white',
+                  'exit light red', 'alarm light', 'heater', 'relais 7']
+
+        for index, name in enumerate(relais):
+            print "%d: %s" % (index,name)
